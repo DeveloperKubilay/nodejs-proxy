c53a8aea3e99ed566b3db9615d61dc61c958798e
Kubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2024-06-02 12:53:06 +0300

    Create http-proxy-client.js

---
+++ http-proxy-client.js
@@ -0,0 +1,54 @@
+var port = 100
+var url = "http://localhost"
+
+const socketio = require('socket.io-client')
+const net = require('net');
+
+
+net.createServer((sourceSocket) => {
+  const io = socketio(url);
+  io.on('connect', () => {
+    console.log(io.id)
+    io.on("data",((msg) =>sourceSocket.write(msg)))
+    io.on("end",io.disconnect)
+    sourceSocket.on('data', (data) => io.emit('zina', data));
+  })
+  io.on("disconnect", () => sourceSocket.end());
+}).listen(port);
+
+
+
+/*
+setTimeout(()=>{
+
+  async function send(){
+    const res = await axios.get('http://ifconfig.me', {
+    proxy: {
+      host: 'localhost',
+      port: 100
+    }
+  }).catch((e)=>{
+   console.log(e.message)
+  })
+  console.log("res"+res?.data)
+  }
+  send()
+},1000)
+*/
+
+
+/*
+
+const puppeteer = require('puppeteer');
+
+(async() => {
+  const browser = await puppeteer.launch({
+    headless:false,
+    args: [ '--proxy-server=127.0.0.1:100' ]
+  });
+  const page = await browser.newPage();
+  await page.goto('http://ifconfig.me');
+  
+})();
+
+*/

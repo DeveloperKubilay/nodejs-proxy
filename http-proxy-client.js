1defe92f1edfc7a307ccd8a189f75d470fe5f37f
Kubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2026-06-03 20:31:38 +0300

    Update http-proxy-client.js

--- http-proxy-client.js
+++ http-proxy-client.js
@@ -1,4 +1,60 @@
-const port = 100
+import net from 'net';
+import socketio from 'socket.io-client';
+import { chromium } from "playwright";
+
+const LOCAL_PORT = 100;
+const SERVER_URL = "http://localhost";
+
+const AUTH = {
+    username: "kullanici1",
+    password: "test"
+};
+
+net.createServer((sourceSocket) => {
+    const io = socketio(SERVER_URL, {
+        auth: AUTH
+    });
+
+    io.on('connect', () => {
+        console.log("Connected to remote:", io.id);
+
+        io.on("data", (msg) => sourceSocket.write(msg));
+        io.on("end", () => io.disconnect());
+
+        sourceSocket.on('data', (data) => io.emit('data2', data));
+    });
+
+    io.on("connect_error", (err) => {
+        console.error("Auth Error:", err.message);
+        sourceSocket.end();
+    });
+
+    io.on("disconnect", () => sourceSocket.end());
+    sourceSocket.on('error', () => io.disconnect());
+
+}).listen(LOCAL_PORT, () => console.log(`Local proxy on ${LOCAL_PORT}`));
+
+const browser = await chromium.launch({
+    proxy: {
+        server: `localhost:${LOCAL_PORT}`
+    },
+    headless: false,
+});
+
+const context = await browser.newContext();
+const page = await context.newPage();
+
+try {
+    await page.goto("https://api.ipify.org/?format=json");
+    console.log(await page.textContent("body"));
+} catch (e) {
+    console.log(e);
+}
+
+
+
+
+/*const port = 100
 const url = "http://localhost"
 
 const socketio = require('socket.io-client')
@@ -16,7 +72,7 @@ net.createServer((sourceSocket) => {
   io.on("disconnect", () => sourceSocket.end());
   sourceSocket.on('error', (err) => io.disconnect());
 }).listen(port);
-
+*/
 
 
 /*
